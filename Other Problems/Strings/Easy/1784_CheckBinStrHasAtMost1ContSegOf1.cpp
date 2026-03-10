4cb85a81dcabb01bbb90c83b0d49dbb16271b23d
Vikrant-codes <vikrantja8@gmail.com>
2026-03-10 12:12:48 +0530

    10Mar

--- Other Problems/Strings/Easy/1784_CheckBinStrHasAtMost1ContSegOf1.cpp
+++ Other Problems/Strings/Easy/1784_CheckBinStrHasAtMost1ContSegOf1.cpp
@@ -26,34 +26,41 @@ using namespace std;
 Problem Statement:-
 The problem want to check if the given string has at most one contiguous segment of 1s.
 That is, if a segment of 1s is present in the string at some index range, no other segment of 1s must exist anywhere else.
-Ex- valid cases : "111000", "100", "00"
+Ex- valid cases : "111000", "100"
 invalid cases : "110111", "10101", "10001"
 
-In valid cases, we can only have at most 1 segment of 1s (or no segment at all), i.e. 
-all the 1s of the string must appear together.
+In valid cases, we can only have at most 1 segment of 1s, i.e. all the 1s of the string must appear together.
 */
 
+// Optimal Approach : Time Complexity : O(n) __ Space Complexity : O(1)
+/*
+Intuition :-
+- The string must contain at most 1 segment of 1s.
+- So, we traverse the string and skip the first segment of 1s.
+- Now, after that we traverse the remaining portion of the string and no '1' should appear in this remaining part for 
+  the condition "at most one contiguous segment of ones" to be true.
+- If '1' appears in this remaining part, it means there is more than one segment of ones. So, return false
+- If loop ends normally, return true.
+
+Code Intuition:-
+- Because the string doesn't have leading zeros, if a segment of 1s exists it must start at index 0.
+- Skip the first contiguous segment of 1s. Make the pointer point to the position after the end of the first segment of 1s.
+- Now, from this position, again traverse the string and if any '1' is found, return false.
+*/
 bool checkOnesSegment(string s) {
-    // all the 1s of the string must form a continuous segment
-    // so the string must have only 1 continuous segment of 1s
     int n = s.size();
 
-    int i = 0;              // traversal pointer 
-    
-    // find the first occurrence index of '1'
-    while (i < n && s[i] != '1') i++;
-    // now 1 is at the first '1' character
-    
-    // move ahead this continuous segment of 1s and check if any other segment of 1 is present apart from this segment
-    while (i < n && s[i] == '1') i++;           // skip this segment of 1s
+    int i = 0;
+    // the problem states that s doesn't have leading zeros
+    // so the first segment of 1s (if exist) must start from index 0 
+    // traverse the first segment of 1s and skip this, place the pointer after the end of 1st segment of 1s
+    while (i < n && s[i] == '1') i++;
 
-    // now check for remaining string portion and it shouldn't have any segment of 1s for true condition
-    while (i < n) {
-        if (s[i] == '1')            // a new segment of 1s found
+    // now traverse till string end & no other 1 must apper for the "at most 1 segment of 1s" condition
+    for (i; i < n; i++) 
+        if (s[i] == '1')
             return false;
-        i++;
-    }
-    
+
     return true;
 }
 
