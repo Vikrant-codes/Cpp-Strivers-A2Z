982b595a582635868f65ebb494ba06a87af760cf
Vikrant-codes <vikrantja8@gmail.com>
2026-08-25 22:26:27 +0530

    25Aug

--- Step_1_LearnTheBasics/LearnC++/Basics/Operators.cpp
+++ Step_1_LearnTheBasics/LearnC++/Basics/Operators.cpp
@@ -101,7 +101,8 @@ or to access class members, namespaces, etc.
 
 int main() {
 
-    // Pre-Increment, Post-Increment, Pre-Decrement and Post-Decrement
+    // <-- Pre-Increment, Post-Increment, Pre-Decrement and Post-Decrement -->
+
     /*
     In C++, the increment (++) and decrement (--) operators come in two forms:
         Pre-increment / Pre-decrement: ++x, --x
@@ -158,6 +159,50 @@ int main() {
     // so i-- returns value 0 and then decrements i by 1 making it -1. 
     // this returned 0 works as boolean false and exits the while loop (while loop don't iterate even for once).
     // after the loop, i is printed and its value is -1.
+
+    // <-- Bitwise Shift Operators -->
+
+    // left shift (<<) : shifts the bits to the left by specified number of positions, fill the vacant places by zero.
+
+    // right shift (>>) : shifts the bits to the left by specified number of positions, 
+    // the vacant places introduced due to shifting are filled with the MSB of original number,
+    // thus if original number was negative (MSB is 1), doing (>>), fills the vacant places with 1
+    // for positive number (MSB is 0), so right shift fills the vacant places with 0
+
+    // left shift by 1 bit operation is equivalent to multiplying by 2.
+    // right shift by 1 bit operation is equivalent to dividing by 2.
+
+    // thus, left shift `n` bits -> equivalent to -> multiplying by 2^n
+    // right shift `n` bits -> equivalent to -> dividing by 2^n
+
+    int b = 1;
+    b = b << 31;    
+    // bits are shifted 31 positions to left, initially b was 1, in binary terms: 00000000 00000000 00000000 00000001
+    // left shifting by 31 bits made it:- 10000000 00000000 00000000 00000000
+    // Now, this new value has MSB set to 1, which means it is a negative number, 
+    // the compiler will thus now read it as -2147483648 (which is INT_MIN)
+    // we can also understanding it as, left shift 31 bits is equivalent to multiplying by 2^31.
+    // now, 1 * 2^31 = 1 * 2147483648 = 2147483648, but this value is beyond int range, 
+    // so, the value is wrapped around and the value becomes -2147483648.
+
+    cout << b << endl;      // -2147483648
+
+    // now any further left shifts will results in zero
+    b = b << 1;     // again doing left shift, makes the MSB 1 bit go to left and hence discarded, the number becomes 0
+    cout << b << endl;      // 0
+
+    // so, left shifting `1` total 31 + 1 = 32 bits gave us zero, but we performed it in two steps, first 31 shifts, then 1 shift
+
+    // In C++, trying to left shift a standard 32-bit int by 32 bits or more results in Undefined Behavior (UB).
+    // so we can't guarantee what happens when we do int_var << 32
+
+    int c = 1;
+    c = c << 32;
+    // compiler gives warning: `warning: left shift count >= width of type [-Wshift-count-overflow]` 
+    cout << c << endl;      // undefined behavior
+
+    unsigned int a = 2147483648;        // 2147483648 = 2 ^ 31
+    cout << (a >> 30) << endl;          // equivalent to 2147483648 / (2 ^ 30) = (2 ^ 31) / (2 ^ 30) = 2
 
     return 0;
 }
